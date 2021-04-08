5197063e286ec6ed79d2b61404f8fa112e57c676
Piotr Jurewicz <piotr.jurewicz.91@gmail.com>
2021-04-08 13:50:34 +0200

    Add testing source file and test executable

---
+++ CMakeFiles/CMakeFiles/Tests.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../build/Tests"
+  "../build/Tests.pdb"
+  "CMakeFiles/Tests.dir/uts/ut.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
