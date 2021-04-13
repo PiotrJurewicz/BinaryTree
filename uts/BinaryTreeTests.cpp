8a0440b9ffb44debaa4d1a32539568f0dcd88352
Piotr Jurewicz <piotr.jurewicz.91@gmail.com>
2021-04-13 18:32:05 +0200

    Add unit tests. Add script for build automatsation

--- uts/BinaryTreeTests.cpp
+++ uts/BinaryTreeTests.cpp
@@ -1,7 +1,71 @@
 #include "BinaryTree.hpp"
 #include "gtest/gtest.h"
 
-TEST(binaryTreeTests, firstTest)
+class binaryTreeTest : public ::testing::Test
+{
+public:
+    BinaryTree binaryTree;
+};
+
+//Sugested test name: When[something]Than[something]Is[something]
+
+TEST_F(binaryTreeTest, WhenAdd0ThanLargerAndSmalerThanValueThanDepthIs2)
+{
+    binaryTree.addNode(0);
+    binaryTree.addNode(1);
+    binaryTree.addNode(-1);
+    ASSERT_EQ(2, binaryTree.returnDepth());
+}
+
+TEST_F(binaryTreeTest, WhenAdd0ThanSmalerAndLargerThanValueThanDepthIs2)
+{
+    binaryTree.addNode(0);
+    binaryTree.addNode(-1);
+    binaryTree.addNode(1);
+    ASSERT_EQ(2, binaryTree.returnDepth());
+}
+
+TEST_F(binaryTreeTest, WhenAdd0ThanSmalerValueThanDepthIs2)
+{
+    binaryTree.addNode(0);
+    binaryTree.addNode(-1);
+    ASSERT_EQ(2, binaryTree.returnDepth());
+}
+
+TEST_F(binaryTreeTest, WhenAdd0ThanLargerValueThanDepthIs2)
+{
+    binaryTree.addNode(0);
+    binaryTree.addNode(1);
+    ASSERT_EQ(2, binaryTree.returnDepth());
+}
+
+TEST_F(binaryTreeTest, WhenAdd3TimesSameValueThanDepthIs3)
+{
+    binaryTree.addNode(0);
+    binaryTree.addNode(0);
+    binaryTree.addNode(0);
+    ASSERT_EQ(3, binaryTree.returnDepth());
+}
+
+TEST_F(binaryTreeTest, WhenAdd2TimesSameValueThanDepthIs2)
+{
+    binaryTree.addNode(0);
+    binaryTree.addNode(0);
+    ASSERT_EQ(2, binaryTree.returnDepth());
+}
+
+TEST_F(binaryTreeTest, WhenAddNodeThanDepthIs1)
+{
+    binaryTree.addNode(0);
+    ASSERT_EQ(1, binaryTree.returnDepth());
+}
+
+TEST_F(binaryTreeTest, WhenTreeCreatedThanDepthIs0)
+{
+    ASSERT_EQ(0, binaryTree.returnDepth());
+}
+
+TEST_F(binaryTreeTest, firstTest)
 {
     ASSERT_TRUE(true);
 }
